e4bc824611cebcb0b0ef612d78fc6e6959ab8232
László Maczó <162982938+dil-lmaczo@users.noreply.github.com>
2024-12-02 11:37:40 +0100

    feat: adds console out

--- C#/Trivia/Tests/First.cs
+++ C#/Trivia/Tests/First.cs
@@ -1,16 +1,47 @@
 using System;
+using System.IO;
 using ApprovalTests;
 using ApprovalTests.Reporters;
+using Trivia;
 using Xunit;
 
+namespace Tests;
+
 [UseReporter(typeof(RiderReporter), typeof(VisualStudioReporter))]
-public class SampleTest
+public class GameLogicTest
 {
+    private static bool _notAWinner;
+    
     [Fact]
-    public void TestList()
+    public void TestLogic()
     {
-        var names = new[] {"Llewellyn", "James", "Dan", "Jason", "Katrina"};
-        Array.Sort(names);
-        Approvals.VerifyAll(names, label: "");
+
+        var output = new StringWriter();
+        Console.SetOut(output);
+        
+        var aGame = new Game();
+
+        aGame.Add("Chet");
+        aGame.Add("Pat");
+        aGame.Add("Sue");
+
+        var rand = new Random(10);
+
+        do
+        {
+            aGame.Roll(rand.Next(5) + 1);
+
+            if (rand.Next(9) == 7)
+            {
+                _notAWinner = aGame.WrongAnswer();
+            }
+            else
+            {
+                _notAWinner = aGame.WasCorrectlyAnswered();
+            }
+        } while (_notAWinner);
+
+        var ASD = output.ToString();
+        Approvals.VerifyAll(ASD, label: "");
     }
 }
